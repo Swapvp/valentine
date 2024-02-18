e15e403ca2f217ab2e68eb78c58831def16ce708
Swapvp <swapneshpawar1994@gmail.com>
2024-02-18 14:55:50 +0530

    fun project

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ let noBtn = document.querySelector("#no-btn");
 let h1 = document.querySelector("h1");
 let imageContainer = document.getElementById("imageContainer");
 
-function noBtnClick() {
+// function noBtnClick() {
+//   var currentSize = window.getComputedStyle(yesBtn).fontSize;
+//   var newSize = parseInt(currentSize) + 10;
+//   yesBtn.style.fontSize = newSize + "px";
+// }
+
+noBtn.addEventListener("click", () => {
   var currentSize = window.getComputedStyle(yesBtn).fontSize;
   var newSize = parseInt(currentSize) + 10;
   yesBtn.style.fontSize = newSize + "px";
-}
+});
 
-function yesBtnClick() {
+yesBtn.addEventListener("click", () => {
   imageContainer.style.display = "block";
-
   noBtn.style.display = "none";
   yesBtn.style.display = "none";
   h1.style.display = "none";
-}
+});
+
+// function yesBtnClick() {
+//   imageContainer.style.display = "block";
+//   noBtn.style.display = "none";
+//   yesBtn.style.display = "none";
+//   h1.style.display = "none";
+// }
